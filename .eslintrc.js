82abea4b5d37ed341595dfd882290333154acda4
Denys <denis.lenskiy1998@gmail.com>
2023-08-31 16:49:02 +0200

    eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
   root: true,
   extends: "@react-native-community",
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   rules: {
     quotes: [2, "double"],
   },
